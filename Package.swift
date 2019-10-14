91f8485f542f7fba4950c39124d637aeecc20625
Venj <ersaclarke@gmail.com>
2019-10-14 17:20:47 +0800

    SwiftPM.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SegmentedControl",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "SegmentedControl",
+            targets: ["SegmentedControl"]),
+    ],
+    targets: [
+        .target(
+            name: "SegmentedControl",
+            path: "SegmentedControl"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
